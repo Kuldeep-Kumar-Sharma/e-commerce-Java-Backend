900deb1852a1d7f9c055cc1f30f38b6a7b0ca163
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-05-20 23:14:10 +0530

    initial commit

---
+++ src/test/java/com/learning/ecommerceweb/ecommbackend/EcommbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.ecommerceweb.ecommbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
